5df3ed97ab6121e7e4180186919ed7ea6cd44983
KacperKwiat <s24533@pjwstk.edu.pl>
2022-05-29 17:52:49 +0200

    php

---
+++ Projekty/Bank_PHP/www/logins/LoginForm.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Golden Horizon Bank</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>login tutorial</title>
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Raleway:100,200,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+if(empty($_SESSION['user'])) {
+    ?>
+    <html>
+
+    <form action="loginin.php" method="post">
+            <input type="text" name="login" placeholder="Enter your login" required>
+            <br>
+            <input type="password" name="password" placeholder="Enter your password" required>
+            <br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </html>
+<?php
+}
+?>
+</body>
+</html>
---
+++ Projekty/Bank_PHP/www/logins/loginin.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$connect= mysqli_connect("127.0.0.1","root","");
+$db=mysqli_select_db($connect,"projekt");
+if(isset($_POST['login'])){
+    $login=$_POST['login'];
+    $password=$_POST['password'];
+    $sql="select *from logins where login ='$login' AND password ='$password'";
+    $result=mysqli_query($db,$sql);
+    if(mysqli_num_rows($result)==1){
+        echo "You did it";
+    }else{
+        echo "You have entered wrong password or login";
+    }
+}else{
+    echo "Nie przeslano zadnych danych";
+}
+
+
+?>
